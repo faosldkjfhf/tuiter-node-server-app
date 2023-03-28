8581b79a1936dd83b743488bf1aeb059a36aae1f
faosldkjfhf <60448325+faosldkjfhf@users.noreply.github.com>
2023-03-27 20:48:49 -0400

    Updated

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 import express from "express";
-const app = express()
-app.get('/hello', (req, res) => { res.send('Hello World!') })
-app.get('/', (req, res) => { res.send('Welcome to Full Stack Development!') })
+import cors from "cors";
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+TuitsController(app);
+HelloController(app);
+UserController(app);
 app.listen(4000)

aa373aa52426b66ef6ffdcd24be738bf9a8aba2d
faosldkjfhf <60448325+faosldkjfhf@users.noreply.github.com>
2023-04-06 14:43:22 -0400

    a9

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ import cors from "cors";
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
 const app = express();
 app.use(cors());
 app.use(express.json());
 TuitsController(app);
 HelloController(app);
 UserController(app);
-app.listen(process.env.PORT || 4000);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+console.log(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING);
